ea5cbde650c33bdf1e2e3308d62cbdadf155d96e
Eleanor <eleanor.ruse@hotmail.co.uk>
2021-05-19 20:13:55 +0100

    Basic Search Engine Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Banner from './Banner.gif';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={Banner} className="Ele-logo" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Eleanor Ruse</small>
+        </footer>
+    </div>
     </div>
   );
 }
