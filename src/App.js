400ecb7e49aa4c6ffdd222c79d8b1343728da219
Eleanor <eleanor.ruse@hotmail.co.uk>
2021-05-19 19:50:20 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Banner from './Banner.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Banner} className="Ele-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
